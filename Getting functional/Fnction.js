e393a690d83b2b56f85dbf125914f772cd6460b7
bellosillo05 <rickyldorado032@gmail.com>
2025-03-27 14:06:26 +0800

    update

---
+++ Getting functional/Fnction.js
@@ -0,0 +1,10 @@
+function bark(name, weight) {
+    if (weight > 30) {
+        console.log(" says WOOF WOOF");
+    } else {
+        console.log(" says woof woof");
+    }
+}
+
+bark("browny", 31);
+bark("whitey", 28);
